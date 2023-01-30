0a58dc0f47aee27cd6f72a05bd5045718fe25a02
wonyongkoon <dnjs0397@naver.com>
2023-01-31 00:04:44 +0900

    [추가] 서버 기본 구성

--- client/src/Page/MainPage.js
+++ client/src/Page/MainPage.js
@@ -4,7 +4,17 @@ import '../Style/MainPage.css'
 
 const MainPage = () => {
   return (
-    <div className='MainPage'>메인페이지 입니다</div>
+    <div className='MainPage'>
+      <div className='MainPage_content'>
+        메인페이지 컨텐츠1
+      </div>
+      <div className='MainPage_content'>
+        메인페이지 컨텐츠2
+      </div>
+      <div className='MainPage_content'>
+        메인페이지 컨텐츠3
+      </div>
+    </div>
   )
 }
 
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+const cookieParser = require('cookie-parser');
+
+dotenv.config();            // env 파일 사용
+app.use(express.json({limit: '100mb'}));   // express에서 body에 json을 읽어오기 위해 선언
+app.use(cookieParser());
+app.use(cors({origin:true, credentials: true}));  // 다른 서버와 통신하려면 cors 필요 
+app.use(express.urlencoded({limit: '100mb', extended: false}));
+const PORT = process.env.PORT;
+
+app.listen(PORT, function(){
+    console.log(`${PORT} 서버에 연결되었습니다.`)
+});
+
+// app.use('/user',require('./routes/user.route')) // 유저
